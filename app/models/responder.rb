4b64b3812be34ede323f0a16dc2e510d20048eb1
Max Schram <maxbschram@gmail.com>
2015-04-18 02:39:37 +0000

    Adds creating responders

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,3 +1,9 @@
 class Responder < ActiveRecord::Base
+  # Disable STI to allow use of 'type' column
+  self.inheritance_column = nil
+  
+  def as_json
+    super(only: [:emergency_code, :type, :name, :capacity, :on_duty])
+  end
   
 end
