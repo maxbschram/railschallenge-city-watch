98348cffb73189ceb83004e30410ad8712ca213b
Max Schram <maxbschram@gmail.com>
2015-04-18 02:49:19 +0000

    Add validation for responder capacity inclusion

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -2,6 +2,9 @@ class Responder < ActiveRecord::Base
   # Disable STI to allow use of 'type' column
   self.inheritance_column = nil
   
+  validates :capacity, inclusion: 1..5
+  
+  
   def as_json
     super(only: [:emergency_code, :type, :name, :capacity, :on_duty])
   end
