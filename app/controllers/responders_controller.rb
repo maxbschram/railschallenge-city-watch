c47676230367a8bd40672e8756ae51ca8dc61881
Max Schram <max@gmail.com>
2015-04-18 02:39:37 +0000

    Adds creating responders

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,4 +1,17 @@
 class RespondersController < ApplicationController
   def create
+    @responder = Responder.new(user_params)
+    if @responder.save
+      render json: {responder: @responder.as_json}
+    else
+      # Error
+    end
   end
+  
+  private
+  
+  def user_params
+    params.require(:responder).permit(:type, :name, :capacity)
+  end
+  
 end
