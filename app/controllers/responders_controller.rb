7b07e2cc13548b3cc7b9b3a14e80c8c9c2965cf3
Max Schram <maxbschram@gmail.com>
2015-04-18 23:55:08 +0000

    Handles unpermitted parameters for responder

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,17 +1,20 @@
 class RespondersController < ApplicationController
   def create
-    @responder = Responder.new(user_params)
-    if @responder.save
-      render json: {responder: @responder.as_json}
+    responder = Responder.new(create_params)
+    
+    if responder.save
+      render json: {responder: responder.as_json}
     else
-      render json: {message: @responder.errors.messages}, status: 422
+      render json: {message: responder.errors.messages}, status: 422
     end
+    
+  rescue ActionController::UnpermittedParameters => e
+      render json: {message: e.message}, status: 422
   end
   
   private
   
-  def user_params
-    params.require(:responder).permit(:type, :name, :capacity)
-  end
-  
+  def create_params
+    params.require(:responder).permit(:name, :type, :capacity)
+  end  
 end
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -6,7 +6,6 @@ class Responder < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :type, presence: true
   
-  
   def as_json
     super(only: [:emergency_code, :type, :name, :capacity, :on_duty])
   end
