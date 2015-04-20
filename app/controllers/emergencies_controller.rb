fd4181e89f5d8249cc846b5141e99044d36c6fe2
Max Schram <maxbschram@gmail.com>
2015-04-20 00:30:59 +0000

    Passes emergency create and show test

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,4 +1,29 @@
 class EmergenciesController < ApplicationController
   def create
+    emergency = Emergency.new(create_params)
+    
+    if emergency.save
+      render json: {emergency: emergency.as_json}, status: 201
+    else
+      render json: {message: emergency.errors.messages}, status: 422
+    end  
+    
+    rescue ActionController::UnpermittedParameters => e
+      render json: {message: e.message}, status: 422
+  end
+  
+  def show
+    emergency = Emergency.find_by(code: params[:id])
+    if emergency == nil
+      render file: 'public/404.json', status: :not_found
+    else    
+      render json: {emergency: emergency.as_json}
+    end
+  end
+  
+  private
+  
+  def create_params
+    params.require(:emergency).permit(:code, :fire_severity, :police_severity, :medical_severity)
   end
 end
--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,3 +1,7 @@
 class Emergency < ActiveRecord::Base
+  validates :code, presence: true, uniqueness: true
+  validates :fire_severity, :police_severity, :medical_severity, presence: true
+  validates_numericality_of :fire_severity, :police_severity, :medical_severity, greater_than_or_equal_to: 0
+  
   has_many :responders
 end
